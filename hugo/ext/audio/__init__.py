1f2d334d647800d902594120bde477a6392f793a
Nariman Safiulin <woofilee@gmail.com>
2018-09-09 13:40:59 +0200

    Joining and leaving channel

--- hugo/ext/audio/__init__.py
+++ hugo/ext/audio/__init__.py
@@ -21,4 +21,71 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
+import re
+
+import discord
+
+from hugo.core.constants import EventType
+from hugo.core.context import Context
+from hugo.core.handler import channel_type, event, not_authored_by_bot, pattern
+from hugo.core.middleware import (
+    MiddlewareState,
+    OneOfAll,
+    collection_of,
+    chain_of,
+)
+
+
 __version__ = "1.0.0"
+
+
+@event(EventType.MESSAGE)
+@not_authored_by_bot()
+@channel_type(guild=True)
+@pattern(re.compile(r"join", re.I))
+async def join(*args, ctx: Context, next, **kwargs):
+    """Join to the user's voice channel."""
+    message = ctx.kwargs["message"]
+    author = message.author
+
+    if not isinstance(author, discord.Member):
+        await message.channel.send("You're not a member of this guild.")
+        return
+    if not author.voice:
+        await message.channel.send("You're not in a voice channel.")
+        return
+    #
+    voice_channel = author.voice.channel
+    voice_client = message.guild.voice_client
+
+    if voice_client is None:
+        voice_client = await voice_channel.connect()
+    else:
+        if voice_client.channel == voice_channel:
+            await message.channel.send("I'm already in your voice channel.")
+            return
+        await voice_client.move_to(voice_channel)
+    #
+    await message.channel.send("Connected!")
+
+
+@event(EventType.MESSAGE)
+@not_authored_by_bot()
+@channel_type(guild=True)
+@pattern(re.compile(r"leave", re.I))
+async def leave(*args, ctx: Context, next, **kwargs):
+    """Leave currently connected voice channel."""
+    message = ctx.kwargs["message"]
+    voice_client = message.guild.voice_client
+
+    if voice_client is None:
+        await message.channel.send("I'm not connected to any of!")
+        return
+    #
+    await voice_client.disconnect(force=True)
+    await message.channel.send("Disconnected!")
+
+
+def get_root_middleware():
+    """Return root middleware chain."""
+    return collection_of(OneOfAll, [join, leave])
